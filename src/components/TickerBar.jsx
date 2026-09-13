7b0ca1d5a7d6305c87834d97d4c1079a1bdb1e57
lovish0001 <7696859022garg@gmail.com>
2026-09-13 20:02:34 +0530

    Use formatPrice for stock price display

--- src/components/TickerBar.jsx
+++ src/components/TickerBar.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import { useCurrency } from './CurrencyContext';
 
 export default function TickerBar({ stocks = [] }) {
   const tickerItems = [...stocks, ...stocks, ...stocks];
+  const { formatPrice } = useCurrency();
 
   return (
     <div className="ticker-bar-container">
@@ -9,7 +10,7 @@ export default function TickerBar({ stocks = [] }) {
         {tickerItems.map((stock, idx) => (
           <div key={`${stock.symbol}-${idx}`} className="ticker-item">
             <span>{stock.symbol}</span>
-            <span>${stock.price.toFixed(2)}</span>
+            <span>{formatPrice(stock.price)}</span>
             <span className={stock.change >= 0 ? 'text-green' : 'text-red'}>
               {stock.change >= 0 ? '+' : ''}{stock.change}%
             </span>
